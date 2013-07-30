141a29e07337a5eac5183db39fa93abac09bbcc6
cagdasc <cagdascaglak@gmail.com>
2013-07-31 00:36:28 +0300

    Add getMotion6() funcion.

--- MPU6050.h
+++ MPU6050.h
@@ -107,6 +107,8 @@ public:
     int16_t getAngularVelocityX();
     int16_t getAngularVelocityY();
     int16_t getAngularVelocityZ();
+    
+    void getMotions6(int16_t *ax, int16_t *ay, int16_t *az, int16_t *gx, int16_t *gy, int16_t *gz);
 
     int16_t getTemperature();
 
