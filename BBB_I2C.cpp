17b8d6b372e0046afc79bb8f667bc0cbd706e905
cagdasc <cagdascaglak@gmail.com>
2013-08-03 00:58:18 +0300

    Source File 1

--- BBB_I2C.cpp
+++ BBB_I2C.cpp
@@ -4,6 +4,8 @@
  * E-mail: cagdascaglak@gmail.com
  * Code is free but There is not warranty.
  * Created on July 26, 2013, 5:01 PM
+ * ----------------------------------------
+ * 03/08/2013 ...NoExit() methods added and it doesn't include exit(1) function. Add Device Address in error messages.
  */
 
 #include "BBB_I2C.h"
@@ -18,18 +20,32 @@ BBB_I2C::~BBB_I2C() {
 }
 
 void BBB_I2C::writeBit(char DEV_ADD, char DATA_REGADD, char value, int bitNum, int bus) {
-    int8_t temp = readByte(DEV_ADD,DATA_REGADD,bus);
+    int8_t temp = readByte(DEV_ADD, DATA_REGADD, bus);
     if (value == 0) {
         temp = temp & ~(1 << bitNum);
     } else if (value == 1) {
         temp = temp | (1 << bitNum);
     } else {
-        printf("Value must be 0 or 1!");
+        printf("Value must be 0 or 1! --> Address %d.\n",DEV_ADD);
         exit(1);
     }
-    
+
     writeByte(DEV_ADD, DATA_REGADD, temp, bus);
-    
+
+}
+
+void BBB_I2C::writeBitNoExit(char DEV_ADD, char DATA_REGADD, char value, int bitNum, int bus) {
+    int8_t temp = readByte(DEV_ADD, DATA_REGADD, bus);
+    if (value == 0) {
+        temp = temp & ~(1 << bitNum);
+    } else if (value == 1) {
+        temp = temp | (1 << bitNum);
+    } else {
+        printf("Value must be 0 or 1! --> Address %d.\n",DEV_ADD);
+    }
+
+    writeByte(DEV_ADD, DATA_REGADD, temp, bus);
+
 }
 
 void BBB_I2C::writeByte(char DEV_ADD, char DATA_REGADD, char value, int bus) {
@@ -40,12 +56,12 @@ void BBB_I2C::writeByte(char DEV_ADD, char DATA_REGADD, char value, int bus) {
     int file;
 
     if ((file = open(path, O_RDWR)) < 0) {
-        printf("%s do not open.\n", path);
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
         exit(1);
     }
 
     if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
-        printf("Can not join I2C Bus\n");
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
         exit(1);
     }
 
@@ -55,8 +71,36 @@ void BBB_I2C::writeByte(char DEV_ADD, char DATA_REGADD, char value, int bus) {
     buffer[1] = value;
 
     if (write(file, buffer, 2) != 2) {
-        printf("Can not write data.\n");
+        printf("Can not write data. Address %d.\n", DEV_ADD);
         exit(1);
+    }
+
+    close(file);
+
+}
+
+void BBB_I2C::writeByteNoExit(char DEV_ADD, char DATA_REGADD, char value, int bus) {
+    char path[20];
+
+    sprintf(path, "/dev/i2c-%d", bus);
+
+    int file;
+
+    if ((file = open(path, O_RDWR)) < 0) {
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
+    }
+
+    if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
+    }
+
+    uint8_t buffer[2];
+
+    buffer[0] = DATA_REGADD;
+    buffer[1] = value;
+
+    if (write(file, buffer, 2) != 2) {
+        printf("Can not write data. Address %d.\n", DEV_ADD);
     }
 
     close(file);
@@ -71,26 +115,55 @@ void BBB_I2C::writeByteBuffer(char DEV_ADD, char DATA_REGADD, uint8_t *value, ui
     int file;
 
     if ((file = open(path, O_RDWR)) < 0) {
-        printf("%s do not open.\n", path);
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
         exit(1);
     }
 
     if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
-        printf("Can not join I2C Bus\n");
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
         exit(1);
     }
-    
+
     char buffer[1];
     buffer[0] = DATA_REGADD;
 
     if (write(file, buffer, 1) != 1) {
-        printf("Can not write data.\n");
+        printf("Can not write data. Address %d.\n", DEV_ADD);
         exit(1);
     }
 
     if (write(file, value, length) != length) {
-        printf("Can not write data.\n");
+        printf("Can not write data. Address %d.\n", DEV_ADD);
         exit(1);
+    }
+
+    close(file);
+}
+
+void BBB_I2C::writeByteBufferNoExit(char DEV_ADD, char DATA_REGADD, uint8_t *value, uint8_t length, int bus) {
+    char path[20];
+
+    sprintf(path, "/dev/i2c-%d", bus);
+
+    int file;
+
+    if ((file = open(path, O_RDWR)) < 0) {
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
+    }
+
+    if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
+    }
+
+    char buffer[1];
+    buffer[0] = DATA_REGADD;
+
+    if (write(file, buffer, 1) != 1) {
+        printf("Can not write data. Address %d.\n", DEV_ADD);
+    }
+
+    if (write(file, value, length) != length) {
+        printf("Can not write data. Address %d.\n", DEV_ADD);
     }
 
     close(file);
@@ -104,12 +177,12 @@ void BBB_I2C::writeByteArduino(char DEV_ADD, int8_t value, int bus) {
     int file;
 
     if ((file = open(path, O_RDWR)) < 0) {
-        printf("%s do not open.\n", path);
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
         exit(1);
     }
 
     if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
-        printf("Can not join I2C Bus\n");
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
         exit(1);
     }
 
@@ -118,8 +191,34 @@ void BBB_I2C::writeByteArduino(char DEV_ADD, int8_t value, int bus) {
     buffer[0] = value;
 
     if (write(file, buffer, 1) != 1) {
-        printf("Can not write data.\n");
+        printf("Can not write data. Address %d.\n", DEV_ADD);
         exit(1);
+    }
+
+    close(file);
+
+}
+void BBB_I2C::writeByteArduinoNoExit(char DEV_ADD, int8_t value, int bus) {
+    char path[20];
+
+    sprintf(path, "/dev/i2c-%d", bus);
+
+    int file;
+
+    if ((file = open(path, O_RDWR)) < 0) {
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
+    }
+
+    if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
+    }
+
+    int8_t buffer[1];
+
+    buffer[0] = value;
+
+    if (write(file, buffer, 1) != 1) {
+        printf("Can not write data. Address %d.\n", DEV_ADD);
     }
 
     close(file);
@@ -134,18 +233,44 @@ void BBB_I2C::writeByteBufferArduino(char DEV_ADD, uint8_t *value, uint8_t buff_
     int file;
 
     if ((file = open(path, O_RDWR)) < 0) {
-        printf("%s do not open.\n", path);
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
         exit(1);
     }
 
     if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
-        printf("Can not join I2C Bus\n");
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
         exit(1);
     }
 
 
     if (write(file, value, buff_len) != buff_len) {
-        printf("Can not write data.\n");
+        printf("Can not write data. Address %d.\n", DEV_ADD);
+        exit(1);
+    }
+
+    close(file);
+}
+
+void BBB_I2C::writeByteBufferArduinoNoExit(char DEV_ADD, uint8_t *value, uint8_t buff_len, int bus) {
+    char path[20];
+
+    sprintf(path, "/dev/i2c-%d", bus);
+
+    int file;
+
+    if ((file = open(path, O_RDWR)) < 0) {
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
+        exit(1);
+    }
+
+    if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
+        exit(1);
+    }
+
+
+    if (write(file, value, buff_len) != buff_len) {
+        printf("Can not write data. Address %d.\n",DEV_ADD);
         exit(1);
     }
 
@@ -153,7 +278,7 @@ void BBB_I2C::writeByteBufferArduino(char DEV_ADD, uint8_t *value, uint8_t buff_
 }
 
 uint8_t BBB_I2C::readBit(char DEV_ADD, char DATA_REGADD, uint8_t bitNum, int bus) {
-    int8_t temp = readByte(DEV_ADD,DATA_REGADD,bus);
+    int8_t temp = readByte(DEV_ADD, DATA_REGADD, bus);
     return (temp >> bitNum) % 2;
 }
 
@@ -165,12 +290,12 @@ int8_t BBB_I2C::readByte(char DEV_ADD, char DATA_REGADD, int bus) {
     int file;
 
     if ((file = open(path, O_RDWR)) < 0) {
-        printf("%s do not open.\n", path);
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
         exit(1);
     }
 
     if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
-        printf("Can not join I2C Bus\n");
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
         exit(1);
     }
 
@@ -179,15 +304,49 @@ int8_t BBB_I2C::readByte(char DEV_ADD, char DATA_REGADD, int bus) {
     buffer[0] = DATA_REGADD;
 
     if (write(file, buffer, 1) != 1) {
-        printf("Can not write data.\n");
+        printf("Can not write data. Address %d.\n", DEV_ADD);
         exit(1);
     }
 
     int8_t value[1];
 
     if (read(file, value, 1) != 1) {
-        printf("Can not read data.\n");
+        printf("Can not read data. Address %d.\n", DEV_ADD);
         exit(1);
+    }
+
+    close(file);
+
+    return value[0];
+}
+
+int8_t BBB_I2C::readByteNoExit(char DEV_ADD, char DATA_REGADD, int bus) {
+    char path[20];
+
+    sprintf(path, "/dev/i2c-%d", bus);
+
+    int file;
+
+    if ((file = open(path, O_RDWR)) < 0) {
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
+    }
+
+    if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
+    }
+
+    uint8_t buffer[1];
+
+    buffer[0] = DATA_REGADD;
+
+    if (write(file, buffer, 1) != 1) {
+        printf("Can not write data. Address %d.\n", DEV_ADD);
+    }
+
+    int8_t value[1];
+
+    if (read(file, value, 1) != 1) {
+        printf("Can not read data. Address %d.\n", DEV_ADD);
     }
 
     close(file);
@@ -203,12 +362,12 @@ void BBB_I2C::readByteBuffer(char DEV_ADD, char DATA_REGADD, uint8_t *data, uint
     int file;
 
     if ((file = open(path, O_RDWR)) < 0) {
-        printf("%s do not open.\n", path);
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
         exit(1);
     }
 
     if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
-        printf("Can not join I2C Bus\n");
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
         exit(1);
     }
 
@@ -217,12 +376,12 @@ void BBB_I2C::readByteBuffer(char DEV_ADD, char DATA_REGADD, uint8_t *data, uint
     buffer[0] = DATA_REGADD;
 
     if (write(file, buffer, 1) != 1) {
-        printf("Can not write data.\n");
+        printf("Can not write data. Address %d.\n", DEV_ADD);
         exit(1);
     }
-    
+
     if (read(file, data, length) != length) {
-        printf("Can not read data.\n");
+        printf("Can not read data. Address %d.\n", DEV_ADD);
         exit(1);
     }
 
@@ -230,26 +389,80 @@ void BBB_I2C::readByteBuffer(char DEV_ADD, char DATA_REGADD, uint8_t *data, uint
 
 }
 
-void BBB_I2C::readByteBufferArduino(char DEV_ADD, uint8_t* data, uint8_t length, int bus) {
-        char path[20];
+void BBB_I2C::readByteBufferNoExit(char DEV_ADD, char DATA_REGADD, uint8_t *data, uint8_t length, int bus) {
+    char path[20];
 
     sprintf(path, "/dev/i2c-%d", bus);
 
     int file;
 
     if ((file = open(path, O_RDWR)) < 0) {
-        printf("%s do not open.\n", path);
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
+    }
+
+    if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
+    }
+
+    uint8_t buffer[1];
+
+    buffer[0] = DATA_REGADD;
+
+    if (write(file, buffer, 1) != 1) {
+        printf("Can not write data. Address %d.\n", DEV_ADD);
+    }
+
+    if (read(file, data, length) != length) {
+        printf("Can not read data. Address %d.\n", DEV_ADD);
+    }
+
+    close(file);
+
+}
+
+void BBB_I2C::readByteBufferArduino(char DEV_ADD, uint8_t* data, uint8_t length, int bus) {
+    char path[20];
+
+    sprintf(path, "/dev/i2c-%d", bus);
+
+    int file;
+
+    if ((file = open(path, O_RDWR)) < 0) {
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
         exit(1);
     }
 
     if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
-        printf("Can not join I2C Bus\n");
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
         exit(1);
     }
-    
+
     if (read(file, data, length) != length) {
-        printf("Can not read data.\n");
+        printf("Can not read data. Address %d.\n", DEV_ADD);
         exit(1);
+    }
+
+    close(file);
+
+}
+
+void BBB_I2C::readByteBufferArduinoNoExit(char DEV_ADD, uint8_t* data, uint8_t length, int bus) {
+    char path[20];
+
+    sprintf(path, "/dev/i2c-%d", bus);
+
+    int file;
+
+    if ((file = open(path, O_RDWR)) < 0) {
+        printf("%s do not open. Address %d.\n", path, DEV_ADD);
+    }
+
+    if (ioctl(file, I2C_SLAVE, DEV_ADD) < 0) {
+        printf("Can not join I2C Bus. Address %d.\n", DEV_ADD);
+    }
+
+    if (read(file, data, length) != length) {
+        printf("Can not read data. Address %d.\n", DEV_ADD);
     }
 
     close(file);
@@ -267,4 +480,3 @@ int16_t BBB_I2C::readWord(char DEV_ADD, uint8_t MSB, uint8_t LSB, int bus) {
 
     return msb;
 }
-
