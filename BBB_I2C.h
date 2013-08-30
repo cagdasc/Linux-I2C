e0e9d03b770b346fcb724aeb03edaf4f561962e2
caca <caca@caca>
2013-08-31 00:01:49 +0300

    Header File 1

--- BBB_I2C.h
+++ BBB_I2C.h
@@ -23,33 +23,30 @@
 
 class BBB_I2C {
 public:
-    BBB_I2C();
-    BBB_I2C(const BBB_I2C& orig);
-    virtual ~BBB_I2C();
 
-    void writeBit(char DEV_ADD, char DATA_REGADD, char value, int bitNum, int bus);
-    void writeBitNoExit(char DEV_ADD, char DATA_REGADD, char value, int bitNum, int bus);
-    void writeBitsNoExit(char DEV_ADD, char DATA_REGADD, char value, int length, int startBit, int bus);
-    void writeByte(char DEV_ADD, char DATA_REGADD, char value, int bus);
-    void writeByteNoExit(char DEV_ADD, char DATA_REGADD, char value, int bus);
-    void writeByteBuffer(char DEV_ADD, char DATA_REGADD, uint8_t *value, uint8_t length, int bus);
-    void writeByteBufferNoExit(char DEV_ADD, char DATA_REGADD, uint8_t *value, uint8_t length, int bus);
-    void writeByteArduino(char DEV_ADD, int8_t value, int bus);
-    void writeByteArduinoNoExit(char DEV_ADD, int8_t value, int bus);
-    void writeByteBufferArduino(char DEV_ADD, uint8_t *value, uint8_t buff_len, int bus);
-    void writeByteBufferArduinoNoExit(char DEV_ADD, uint8_t *value, uint8_t buff_len, int bus);
+    void writeBit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bitNum, int bus);
+    void writeBitNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bitNum, int bus);
+    void writeBitsNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int length, int startBit, int bus);
+    void writeByte(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bus);
+    void writeByteNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bus);
+    void writeByteBuffer(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *value, uint8_t length, int bus);
+    void writeByteBufferNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *value, uint8_t length, int bus);
+    void writeByteArduino(uint8_t DEV_ADD, int8_t value, int bus);
+    void writeByteArduinoNoExit(uint8_t DEV_ADD, int8_t value, int bus);
+    void writeByteBufferArduino(uint8_t DEV_ADD, uint8_t *value, uint8_t length, int bus);
+    void writeByteBufferArduinoNoExit(uint8_t DEV_ADD, uint8_t *value, uint8_t length, int bus);
 
-    uint8_t readBit(char DEV_ADD, char DATA_REGADD, uint8_t bitNum, int bus);
-    uint8_t readBitNoExit(char DEV_ADD, char DATA_REGADD, uint8_t bitNum, int bus);
-    uint8_t readBitsNoExit(char DEV_ADD, char DATA_REGADD, uint8_t length, uint8_t startBit, int bus);
-    uint8_t readByte(char DEV_ADD, char DATA_REGADD, int bus);
-    uint8_t readByteNoExit(char DEV_ADD, char DATA_REGADD, int bus);
-    void readByteBuffer(char DEV_ADD, char DATA_REGADD, uint8_t *data, uint8_t length, int bus);
-    void readByteBufferNoExit(char DEV_ADD, char DATA_REGADD, uint8_t *data, uint8_t length, int bus);
-    void readByteBufferArduino(char DEV_ADD, uint8_t *data, uint8_t length, int bus);
-    void readByteBufferArduinoNoExit(char DEV_ADD, uint8_t *data, uint8_t length, int bus);
-    int16_t readWord(char DEV_ADD, uint8_t MSB, uint8_t LSB, int bus);
-    int16_t readWordNoExit(char DEV_ADD, uint8_t MSB, uint8_t LSB, int bus);
+    uint8_t readBit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t bitNum, int bus);
+    uint8_t readBitNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t bitNum, int bus);
+    uint8_t readBitsNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t length, uint8_t startBit, int bus);
+    uint8_t readByte(uint8_t DEV_ADD, uint8_t DATA_REGADD, int bus);
+    uint8_t readByteNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, int bus);
+    void readByteBuffer(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *data, uint8_t length, int bus);
+    void readByteBufferNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *data, uint8_t length, int bus);
+    void readByteBufferArduino(uint8_t DEV_ADD, uint8_t *data, uint8_t length, int bus);
+    void readByteBufferArduinoNoExit(uint8_t DEV_ADD, uint8_t *data, uint8_t length, int bus);
+    int16_t readWord(uint8_t DEV_ADD, uint8_t MSB, uint8_t LSB, int bus);
+    int16_t readWordNoExit(uint8_t DEV_ADD, uint8_t MSB, uint8_t LSB, int bus);
 
 private:
 
