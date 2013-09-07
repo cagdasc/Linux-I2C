122a6105e6fff0dec37e101d13201b65e8680d48
caca <caca@caca>
2013-09-07 23:56:32 +0300

    constructor method for i2c bus

--- BBB_I2C.h
+++ BBB_I2C.h
@@ -10,7 +10,6 @@
 #define	BBB_I2C_H
 
 #include <math.h>
-
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
@@ -21,35 +20,39 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define I2C_BUS 1
+
 class BBB_I2C {
 public:
 
-    void writeBit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bitNum, int bus);
-    void writeBitNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bitNum, int bus);
-    void writeBitsNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int length, int startBit, int bus);
-    void writeByte(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bus);
-    void writeByteNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bus);
-    void writeByteBuffer(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *value, uint8_t length, int bus);
-    void writeByteBufferNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *value, uint8_t length, int bus);
-    void writeByteArduino(uint8_t DEV_ADD, int8_t value, int bus);
-    void writeByteArduinoNoExit(uint8_t DEV_ADD, int8_t value, int bus);
-    void writeByteBufferArduino(uint8_t DEV_ADD, uint8_t *value, uint8_t length, int bus);
-    void writeByteBufferArduinoNoExit(uint8_t DEV_ADD, uint8_t *value, uint8_t length, int bus);
+    BBB_I2C();
+    BBB_I2C(uint8_t busAddr);
+    void writeBit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bitNum);
+    void writeBitNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int bitNum);
+    void writeBitsNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value, int length, int startBit);
+    void writeByte(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value);
+    void writeByteNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t value);
+    void writeByteBuffer(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *value, uint8_t length);
+    void writeByteBufferNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *value, uint8_t length);
+    void writeByteArduino(uint8_t DEV_ADD, int8_t value);
+    void writeByteArduinoNoExit(uint8_t DEV_ADD, int8_t value);
+    void writeByteBufferArduino(uint8_t DEV_ADD, uint8_t *value, uint8_t length);
+    void writeByteBufferArduinoNoExit(uint8_t DEV_ADD, uint8_t *value, uint8_t length);
 
-    uint8_t readBit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t bitNum, int bus);
-    uint8_t readBitNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t bitNum, int bus);
-    uint8_t readBitsNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t length, uint8_t startBit, int bus);
-    uint8_t readByte(uint8_t DEV_ADD, uint8_t DATA_REGADD, int bus);
-    uint8_t readByteNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, int bus);
-    void readByteBuffer(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *data, uint8_t length, int bus);
-    void readByteBufferNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *data, uint8_t length, int bus);
-    void readByteBufferArduino(uint8_t DEV_ADD, uint8_t *data, uint8_t length, int bus);
-    void readByteBufferArduinoNoExit(uint8_t DEV_ADD, uint8_t *data, uint8_t length, int bus);
-    int16_t readWord(uint8_t DEV_ADD, uint8_t MSB, uint8_t LSB, int bus);
-    int16_t readWordNoExit(uint8_t DEV_ADD, uint8_t MSB, uint8_t LSB, int bus);
+    uint8_t readBit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t bitNum);
+    uint8_t readBitNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t bitNum);
+    uint8_t readBitsNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t length, uint8_t startBit);
+    uint8_t readByte(uint8_t DEV_ADD, uint8_t DATA_REGADD);
+    uint8_t readByteNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD);
+    void readByteBuffer(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *data, uint8_t length);
+    void readByteBufferNoExit(uint8_t DEV_ADD, uint8_t DATA_REGADD, uint8_t *data, uint8_t length);
+    void readByteBufferArduino(uint8_t DEV_ADD, uint8_t *data, uint8_t length);
+    void readByteBufferArduinoNoExit(uint8_t DEV_ADD, uint8_t *data, uint8_t length);
+    int16_t readWord(uint8_t DEV_ADD, uint8_t MSB, uint8_t LSB);
+    int16_t readWordNoExit(uint8_t DEV_ADD, uint8_t MSB, uint8_t LSB);
 
 private:
-
+    uint8_t busAddr;
 };
 
 #endif	/* BBB_I2C_H */
