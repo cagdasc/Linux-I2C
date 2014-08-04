3dfe4f5d3a06b36294e7945503f21718928a5e9c
Cagdas <cagdascaglak@gmail.com>
2014-08-04 08:54:51 +0300

    License Change

--- ADXL345.cpp
+++ ADXL345.cpp
@@ -1,9 +1,19 @@
-/* 
- * File:   ADXL345.cpp
- * Author: caca
- * 
- * Created on August 28, 2013, 7:30 PM
- */
+/*
+    Copyright (C) 2014  Cagdas Caglak cagdascaglak@gmail.com http://expcodes.blogspot.com.tr/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "ADXL345.h"
 
